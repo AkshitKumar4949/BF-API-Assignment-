608c12f833ac780e0779106dfb68c1dc858d637a
Akshit Kumar <akshitkumar4949@gmail.com>
2026-02-10 13:13:54 +0530

    updated

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+const dotenv = require("dotenv");
+dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const express = require("express");
