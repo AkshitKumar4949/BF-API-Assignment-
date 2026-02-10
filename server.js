5478dcc24fd0f865ecc19923ac8af6ffded3fa4a
Akshit Kumar <akshitkumar4949@gmail.com>
2026-02-10 12:57:00 +0530

    Updated

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const express = require("express");
 const app = express();
 
@@ -24,18 +28,18 @@ const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
 const lcm = (a, b) => (a * b) / gcd(a, b);
 
 // POST /bfhl
-app.post("/bfhl", (req, res) => {
+app.post("/bfhl", async (req, res) => {
   try {
     const body = req.body;
     let data = null;
 
     if (body.fibonacci !== undefined) {
       let n = body.fibonacci;
-      let fib = [0,1];
-      for (let i=2;i<n;i++){
-        fib[i] = fib[i-1] + fib[i-2];
+      let fib = [0, 1];
+      for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
       }
-      data = fib.slice(0,n);
+      data = fib.slice(0, n);
     }
 
     else if (body.prime) {
@@ -43,31 +47,36 @@ app.post("/bfhl", (req, res) => {
     }
 
     else if (body.hcf) {
-      data = body.hcf.reduce((a,b)=>gcd(a,b));
+      data = body.hcf.reduce((a, b) => gcd(a, b));
     }
 
     else if (body.lcm) {
-      data = body.lcm.reduce((a,b)=>lcm(a,b));
+      data = body.lcm.reduce((a, b) => lcm(a, b));
     }
 
     else if (body.AI) {
-      data = "Mumbai";
+      const prompt = body.AI;
+
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      data = response.text();
     }
 
+
     else {
-      return res.status(400).json({is_success:false});
+      return res.status(400).json({ is_success: false });
     }
 
     res.json({
-      is_success:true,
-      official_email:"akshit0357.be23@chitkara.edu.in",
-      data:data
+      is_success: true,
+      official_email: "akshit0357.be23@chitkara.edu.in",
+      data: data
     });
 
   } catch {
-    res.status(500).json({is_success:false});
+    res.status(500).json({ is_success: false });
   }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>console.log("Running on",PORT));
+app.listen(PORT, () => console.log("Running on", PORT));
